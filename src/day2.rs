b0251971b40cae681eb6c239d4a0e1531c1c81fb
Fredrik Hagman <fhagman@spotify.com>
2020-12-02 10:49:37 +0100

    Day2

--- src/day2.rs
+++ src/day2.rs
@@ -1,50 +1,54 @@
+use regex::Regex;
 
-fn run(mut program : Vec<i32>) -> i32 {
-    let mut pos : usize = 0;
+struct Line {
+    min: u32,
+    max: u32,
+    ch: String,
+    password: String,
+}
 
-    loop {
-        let p1 : usize = program[pos + 1] as usize;
-        let p2 : usize = program[pos + 2] as usize;
-        let p3 : usize = program[pos + 3] as usize;
-        if program[pos] == 1 {
-            program[p3] = program[p1] + program[p2];
-            pos += 4;
-        } else if program[pos] == 2 {
-            program[p3] = program[p1] * program[p2];
-            pos += 4;
-        } else if program[pos] == 99 {
-            return program[0];
-        } else {
-            panic!("Invalid instruction");
-        }
+fn parse_lines(lines: std::str::Lines) -> Vec<Line> {
+    let mut result : Vec<Line> = Vec::new();
+    // 3-4 j: tjjj
+    let rg = Regex::new(r"(\d+)-(\d+) ([a-z]): (.*)").unwrap();
+    for line in lines {
+        let m = rg.captures(line).unwrap();
+        result.push(Line {min: m[1].parse().unwrap(), max: m[2].parse().unwrap(), ch: m[3].to_string(), password: m[4].to_string()});
     }
-    return 0;
+    return result;
+}
+
+fn valid_password(line : &Line) -> bool {
+    let re = Regex::new(&line.ch).unwrap();
+    let count = re.find_iter(&line.password).count();
+    return line.min as usize <= count && count <= line.max as usize;
+}
+
+fn valid_password2(line : &Line) -> bool {
+    return (line.password.chars().nth((line.min - 1) as usize) == line.ch.chars().nth(0)) ^
+           (line.password.chars().nth((line.max - 1) as usize) == line.ch.chars().nth(0));
 }
 
 pub fn day2() {
-    let mut contents = std::fs::read_to_string("input2.txt")
+    let contents : String = std::fs::read_to_string("input2.txt")
         .expect("Failed to read file");
-    contents.pop();
-    let mut program : Vec<i32> = contents.split(",").map(|x| x.parse::<i32>().unwrap()).collect();
-    
-    //replace position 1 with the value 12 and replace position 2 with the value 2.
-    program[1] = 12;
-    program[2] = 2;
-    let result = run(program);
-    println!("Day 2, part 1: Result: {}", result);
+    let lines_raw: std::str::Lines = contents.lines();
+    let lines : Vec<Line> = parse_lines(lines_raw);
 
-    for noun in 0..100 {
-        for verb in 0..100 {
-            program = contents.split(",").map(|x| x.parse::<i32>().unwrap()).collect();
-            program[1] = noun;
-            program[2] = verb;
-            let result = run(program);
-            if result == 19690720 {
-                println!("Day 2, part 2: Result: {}", 100 * noun + verb);
-                return;
-            }
+    let mut valid = 0;
+    for line in &lines {
+        if valid_password(line) {
+            valid += 1;
         }
     }
-    panic!("No solution found");
+    println!("Day 2, part 1: {}", valid);
+
+    valid = 0;
+    for line in &lines {
+        if valid_password2(line) {
+            valid += 1;
+        }
+    }
+    println!("Day 2, part 2: {}", valid);
 }
 
