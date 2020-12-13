729eea7491ca29505db3f49495246594e62a6445
Fredrik Hagman <fhagman@spotify.com>
2020-12-13 14:46:03 +0100

    day13

---
+++ src/day13.rs
@@ -0,0 +1,59 @@
+fn parse_busses(line : &str) -> Vec<u64> {
+    let mut result : Vec<u64> = Vec::new();
+    for bus in line.split(',') {
+        if bus != "x" {
+            result.push(bus.parse().unwrap());
+        } else {
+            result.push(0);
+        }
+    }
+    return result;
+}
+
+fn part1(time : u64, busses : &Vec<u64>) ->u64 {
+    let mut first_dep = u64::MAX;
+    let mut first_bus = 0;
+    for bus in busses {
+        if *bus > 0 {
+           let dep = bus - (time % bus);
+            if dep < first_dep {
+                first_dep = dep;
+                first_bus = *bus;
+            }
+        }
+    }
+    return first_bus * first_dep;
+}
+
+fn part2(busses : &Vec<u64>) ->u64 {
+    let mut timestamp : u64 = 0;
+    let mut inc : u64 = 1;
+    for offset in 0..(busses.len()) {
+        let bus : u64 = busses[offset];
+        if bus > 0 {
+            loop {
+                timestamp += inc;
+                if (timestamp + (offset as u64)) % bus == 0 {
+                    break;
+                }
+            }
+            inc *= bus
+        }
+    }
+
+    return timestamp;
+}
+
+/*
+Day 13, part 1: 2238
+Day 13, part 2: 560214575859998
+*/
+
+pub fn day13() {
+    let contents = std::fs::read_to_string("input13.txt").expect("Failed to read file");
+    let mut lines = contents.lines();
+    let time : u64 = lines.next().unwrap().parse().unwrap();
+    let busses : Vec<u64> = parse_busses(lines.next().unwrap());
+    println!("Day 13, part 1: {}", part1(time, &busses));
+    println!("Day 13, part 2: {}", part2(&busses));
+  }
